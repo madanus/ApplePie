cb7395a555e4d6470eb20c8f4bbe4292aed4a5f5
Techartus Solutions <madan@techartus.com>
2020-07-09 19:32:24 +0530

    Apple Pie Initial Project

---
+++ ApplePie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplePie
+//
+//  Created by Madan on 08/07/20.
+//  Copyright © 2020 Move Up. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
